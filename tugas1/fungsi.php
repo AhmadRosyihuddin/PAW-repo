07df200ab76d8941b50ef7a66e83d72933d62179
AhmadRosyihuddin <arosyihuddin6@gmail.com>
2022-03-09 23:11:16 -0800

    memperbarui source code fungsi.php

--- tugas1/fungsi.php
+++ tugas1/fungsi.php
@@ -2,7 +2,15 @@
 function penjumlahan($a, $b){
   return $a += $b;
 }
-echo(penjumlahan(32,24));
-echo(penjumlahan(21,41));
-echo(penjumlahan(21,24));
+function pengurangan($a, $b){
+  return $a -= $b;
+}
+function perkalian($a, $b){
+  return $a -= $b;
+}
+$nilai1 = 41;
+$nilai2 = 12;
+echo("hasil dari $nilai1 + $nilai2 = ".penjumlahan($nilai1,$nilai2))."<br>";
+echo("hasil dari $nilai1 - $nilai2 = ".pengurangan($nilai1,$nilai2))."<br>";
+echo("hasil dari $nilai1 * $nilai2 = ".perkalian($nilai1,$nilai2))."<br>";
 ?>
