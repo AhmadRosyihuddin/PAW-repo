94eee88deba9d1674d7d54559f9c8ad6f6c95384
AhmadRosyihuddin <arosyihuddin6@gmail.com>
2022-03-31 14:54:42 +0700

    Update Codingan membuat tabel database mahasiswa di tambah dengan tamplate bootstrap

--- Tugas3/function.php
+++ Tugas3/function.php
@@ -13,15 +13,10 @@ function select($query)
     global $koneksi; // membuat variabel koneksi mennjadi global
     $data = mysqli_query($koneksi, $query);
     $rows = []; // menyimpan semua data ke dalam array
-    //jika data lebih dari 1 maka akan di lakukan perulangan dan di simpan di array
-    if ($a = mysqli_num_rows($data) > 1) {
-        while ($row = mysqli_fetch_assoc($data)) {
-            $rows[] = $row;
-        }
-        return $rows;
-    } else { // jika data nya cuma 1 maka langsung di kembalikan 
-        return mysqli_fetch_assoc($data);
+    while ($row = mysqli_fetch_assoc($data)) {
+        $rows[] = $row;
     }
+    return $rows;
 }
 
 // function delete data
@@ -52,5 +47,14 @@ function edit($id_mhs, $value)
     $nim_mhs = $value['nim_mhs'];
     $alamat_mhs = $value['alamat_mhs'];
     $update = mysqli_query($koneksi, "UPDATE tbl_126 SET nama_mhs='$nama_mhs', nim_mhs='$nim_mhs', alamat_mhs='$alamat_mhs' WHERE id_mhs = '$id_mhs'");
+    return mysqli_affected_rows($koneksi);
+}
+
+function choiceDelete($pilihan)
+{
+    global $koneksi;
+    foreach ($pilihan as $id_mhs) {
+        $delete = mysqli_query($koneksi, "DElETE FROM tbl_126 WHERE id_mhs=$id_mhs");
+    }
     return mysqli_affected_rows($koneksi);
 }
--- Tugas3/index.php
+++ Tugas3/index.php
@@ -1,110 +1,209 @@
 <?php
-include 'function.php';
 $id_mhs = $_GET["id_mhs"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tugas PAW</title>
-    <link rel="stylesheet" href="vendor/css/bootstrap.min.css">
-    <script type="text/javascript" src="vendor/js/awesome.js"></script>
-    <script type="text/javascript" src="vendor/js/bootstrap.bundle.min.js"></script>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+	<title>Index1</title>
+	<link rel="stylesheet" href="vendor/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="vendor/css/style.css" />
+	<link rel="stylesheet" href="vendor/css/material.css" />
+	<script src="vendor/js/popper.min.js"></script>
+	<script src="vendor/js/jquery.min.js"></script>
 </head>
 
 <body>
-    <!-- CDN untuk bisa mengakses sweetalert -->
-    <script src="vendor/js/sweetalert.js"></script>
+	<?php include 'alert.php'; ?>
+	<div class="container-xl">
+		<div class="table-responsive">
+			<div class="table-wrapper">
+				<div class="table-title">
+					<div class="row">
+						<div class="col-sm-6">
+							<h2>Tugas PAW 3 <b>Database Mahasiswa</b></h2>
+						</div>
+						<div class="col-sm-6">
+							<a href="#tambahData" class="btn btn-success" data-toggle="modal"><i class="material-icons">&#xE147;</i> <span>Tambah Data</span></a>
+							<a href="#hapusData" class="btn btn-danger" data-toggle="modal"><i class="material-icons">&#xE15C;</i> <span>Hapus</span></a>
+						</div>
+					</div>
+				</div>
+				<?php $row = select("SELECT * FROM tbl_126");
+				if (count($row)) { ?>
+					<form action="" method="POST">
+						<table class="table table-striped table-hover table-bordered">
+							<thead>
+								<tr>
+									<th>
+										<span class="custom-checkbox">
+											<input type="checkbox" id="selectAll" />
+											<label for="selectAll"></label>
+										</span>
+									</th>
+									<th>Nama</th>
+									<th>NIM</th>
+									<th>Alamat</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($row as $data) { ?>
+									<tr>
+										<td>
+											<span class="custom-checkbox">
+												<input type="checkbox" id="<?= $data['id_mhs'] ?>" name="pilihan[]" value="<?= $data['id_mhs'] ?>" />
+												<label for="<?= $data['id_mhs'] ?>"></label>
+											</span>
+										</td>
+										<td><?= $data['nama_mhs']; ?></td>
+										<td><?= $data['nim_mhs']; ?></td>
+										<td><?= $data['alamat_mhs']; ?></td>
+										<td>
+											<a href="#editData<?= $data['id_mhs'] ?>" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+											<a href="index.php?id_mhs=<?= $data['id_mhs'] ?>&hapus=''"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
 
-    <?php
-    // cek di url jika ada hapus maka akan di lakukan penghapusan
-    if (isset($_GET['hapus'])) {
-        if (delete($id_mhs)) { ?>
-            <script language="javascript">
-                Swal.fire({
-                    title: 'SUKSES',
-                    text: 'DATA BERHASIL DI HAPUS',
-                    icon: 'success',
-                    confirmButtonText: 'OK'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        document.location = "index.php"
-                    }
-                })
-            </script>
-        <?php } else { ?>
-            <script language="javascript">
-                document.location = 'index.php';
-                Swal.fire({
-                    title: 'GAGAL',
-                    text: 'DATA GAGAL DI HAPUS',
-                    icon: 'error',
-                    confirmButtonText: 'OK'
-                }).then((result) => {
-                    if (result.isConfirmed) {
-                        document.location = "index.php"
-                    }
-                })
-            </script>
-        <?php }
-    }
-    $row = select("SELECT * FROM tbl_126");
-    if (count($row)) { ?>
-        <table class="table table-hover table-primary" style="text-align:center;">
-            <th colspan="4">Database Mahasiswa</th>
-            <th>
-                <a href="tambah.php" style="text-decoration: none; color:black; ">
-                    <button type="button" class="btn btn-primary">
-                        <i class="fa-solid fa-file-circle-plus"></i>
-                    </button>
-                </a>
-            </th>
-            <tr style="font-weight: bold; text-align:center; color:black;">
-                <td>NO</td>
-                <td>Nama</td>
-                <td>NIM</td>
-                <td>Alamat</td>
-                <td>Opsi</td>
-            </tr>
+											<!-- Edit Modal HTML -->
+											<div id="editData<?= $data['id_mhs'] ?>" class="modal fade">
+												<div class="modal-dialog">
+													<div class="modal-content">
+														<form method="POST">
+															<div class="modal-header">
+																<h4 class="modal-title">Edit Data</h4>
+																<button type="button" class="btn-close" data-dismiss="modal" aria-hidden="true"></button>
+															</div>
+															<div class="modal-body">
+																<input type="hidden" name="id_mhs" value="<?= $data['id_mhs'] ?>">
+																<div class="form-group">
+																	<label>Name</label>
+																	<input type="text" class="form-control" name="nama_mhs" value="<?= $data['nama_mhs']; ?>" required />
+																</div>
+																<div class="form-group">
+																	<label>NIM</label>
+																	<input type="text" class="form-control" name="nim_mhs" value="<?= $data['nim_mhs']; ?>" required />
+																</div>
+																<div class="form-group">
+																	<label>Alamat</label>
+																	<textarea class="form-control" name="alamat_mhs" required><?= $data['alamat_mhs']; ?></textarea>
+																</div>
+															</div>
+															<div class="modal-footer">
+																<input type="button" class="btn btn-default" data-dismiss="modal" value="Batal" />
+																<input type="submit" class="btn btn-success" value="edit" name="edit" />
+															</div>
+														</form>
+													</div>
+												</div>
+											</div>
+											<!-- End Of Modal Edit -->
+										</td>
+									</tr>
+								<?php } ?>
+							</tbody>
+						</table>
+					<?php } else { ?>
+						<table class="table table-striped table-hover table-bordered">
+							<thead>
+								<tr>
+									<th>
+										<span class="custom-checkbox">
+											<input type="checkbox" id="selectAll" />
+											<label for="selectAll"></label>
+										</span>
+									</th>
+									<th>Nama</th>
+									<th>NIM</th>
+									<th>Alamat</th>
+									<th>Actions</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td colspan="5" style="text-align: center;">TIDAK ADA DATA MAHASISWA</td>
+								</tr>
+							</tbody>
+						</table>
+					<?php } ?>
 
-            <?php $i = 1;
-            foreach ($row as $data) { ?>
-                <tr>
-                    <td><?= $i ?></td>
-                    <td><?= $data['nama_mhs']; ?></td>
-                    <td><?= $data['nim_mhs']; ?></td>
-                    <td><?= $data['alamat_mhs']; ?></td>
-                    <td>
-                        <a href="edit.php?id_mhs=<?= $data['id_mhs'] ?>" style="text-decoration: none; color:black;">
-                            <i class="fa-solid fa-pen-to-square"></i>
-                        </a>|
-                        <a href="index.php?id_mhs=<?= $data['id_mhs'] ?>&hapus=''" style="text-decoration: none; color:black;">
-                            <i class="fa-solid fa-trash-can"></i>
-                        </a>
-                    </td>
-                </tr>
-            <?php $i++;
-            } ?>
-        </table>
-    <?php } else { ?>
-        <table border="5" cellspacing="1" cellpadding="10" style="margin:auto">
-            <th colspan="4">Database Mahasiswa</th>
-            <th><a href="inputData.php" style="text-decoration: none; color:black;">Tambah</a></th>
-            <tr style="font-weight: bold; text-align:center; color:black;">
-                <td>NO</td>
-                <td>Nama</td>
-                <td>NIM</td>
-                <td>Alamat</td>
-                <td>Opsi</td>
-            </tr>
-            <tr>
-                <td colspan="5" style="text-align: center;">TIDAK ADA DATA MAHASISWA</td>
-            </tr>
-        </table>
-    <?php } ?>
+					<div class="clearfix">
+						<div class="hint-text">
+							Showing <b>5</b> out of <b>25</b> entries
+						</div>
+						<ul class="pagination">
+							<li class="page-item disabled"><a href="#">Previous</a></li>
+							<li class="page-item"><a href="#" class="page-link">1</a></li>
+							<li class="page-item"><a href="#" class="page-link">2</a></li>
+							<li class="page-item active">
+								<a href="#" class="page-link">3</a>
+							</li>
+							<li class="page-item"><a href="#" class="page-link">4</a></li>
+							<li class="page-item"><a href="#" class="page-link">5</a></li>
+							<li class="page-item"><a href="#" class="page-link">Next</a></li>
+						</ul>
+					</div>
+					<!-- Delete Modal HTML -->
+					<div id="hapusData" class="modal fade">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h4 class="modal-title">Delete Data</h4>
+									<button type="button" class="btn-close" data-dismiss="modal" aria-hidden="true"></button>
+								</div>
+								<div class="modal-body">
+									<p>Apakah Kamu Yakin Mau menghapus Data ini?</p>
+									<p class="text-warning">
+										<small>Jika anda menghapus data ini maka anda tidak dapat mengembalikan nya</small>
+									</p>
+								</div>
+								<div class="modal-footer">
+									<input type="button" class="btn btn-default" data-dismiss="modal" value="Batal" />
+									<input type="submit" class="btn btn-danger" value="Hapus" name="choiceHapus" />
+								</div>
+							</div>
+						</div>
+					</div>
+					</form>
+			</div>
+		</div>
+	</div>
+	<!-- Tambah Modal HTML -->
+	<div id="tambahData" class="modal fade">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<form method="POST">
+					<div class="modal-header">
+						<h4 class="modal-title">Tambah Data</h4>
+						<button type="button" class="btn-close" data-dismiss="modal" aria-hidden="true"></button>
+					</div>
+					<div class="modal-body">
+						<div class="form-group">
+							<label>Name</label>
+							<input type="text" class="form-control" name="nama_mhs" required />
+						</div>
+						<div class="form-group">
+							<label>NIM</label>
+							<input type="text" class="form-control" name="nim_mhs" required />
+						</div>
+						<div class="form-group">
+							<label>Alamat</label>
+							<textarea class="form-control" name="alamat_mhs" required></textarea>
+						</div>
+					</div>
+					<div class="modal-footer">
+						<input type="button" class="btn btn-default" data-dismiss="modal" value="Batal" />
+						<input type="submit" class="btn btn-success" value="Tambah" name="submit" />
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+
+
+	<script src="vendor/js/main.js"></script>
+	<script src="vendor/js/bootstrap.min.js"></script>
 </body>
 
 </html>
